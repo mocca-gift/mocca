c20bc65e6ff24ca3548bcda787468e5a7c9abbeb
Ryoto Arai <arai.ryoto@gmail.com>
2016-05-23 20:45:24 +0000

    talk

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -36,23 +36,74 @@ class WebhookController < ApplicationController
       
     else
       @talk=Talk.find_by(:user => from_mid)
-      qarray=@talk.question.split(",")
-      ansarray=@talk.text.split(",")
+      @qarray=@talk.question.split(",")
+      @ansarray=@talk.text.split(",")
       i=ansarray.count
       if i<5 then
         case text_message
         when "1" then
-          message=Question.find_by_id(qarray[i+1]).body+"\nYESの場合は1をNOの場合は2を返して下さい．"
+          message=Question.find_by_id(@qarray[i+1]).body+"\nYESの場合は1をNOの場合は2を返して下さい．"
           @talk.update(:text => @talk.text+","+text_message)
         when "2" then
-          message=Question.find_by_id(qarray[i+1]).body+"\nYESの場合は1をNOの場合は2を返して下さい．"
+          message=Question.find_by_id(@qarray[i+1]).body+"\nYESの場合は1をNOの場合は2を返して下さい．"
           @talk.update(:text => @talk.text+","+text_message)
         else
           message="1か2で答えてください"
         end
       else
         @talk.update(:text => @talk.text+","+text_message)
-        message=@talk.question+@talk.text
+        
+        # answerモデルに結果を代入
+        # for i in 0..4 do
+        for i in 1..(@ansarray.length-1) do
+            answer=Answer.where(question_id: @qarray[i]).find_by_ansid(@ansarray[i]) || Answer.new(question_id: @qarray[i], ansid: @ansarray[i], count: 0)
+            answer.save
+            #n=answer.count
+            answer.update(count: answer.count+1)
+        end
+        
+        
+        #ベイズ
+        #ベイズ確率の配列作成
+        @bayes=Hash.new(0.5)
+      
+        @gifts=Gift.all
+        
+        #ベイズ計算
+        for i in 1..5 do
+          @gifts.each do |gift|
+            answer=Answer.where(question_id: @qarray[i]).find_by_ansid(@ansarray[i])
+            evaluation1=Evaluation.where(gift_id: gift.id).find_by_evalid(1) || Evaluation.new(gift_id: gift.id, evalid: 1, count: 0)
+            evaluation1.save
+            evaluation2=Evaluation.where(gift_id: gift.id).find_by_evalid(2) || Evaluation.new(gift_id: gift.id, evalid: 2, count: 0)
+            evaluation2.save
+            anstoeval1=Anstoeval.where(answer_id: answer.id).find_by_evaluation_id(evaluation1.id) || Anstoeval.new(answer_id: answer.id, evaluation_id: evaluation1.id, count: 1)
+            anstoeval1.save
+            anstoeval2=Anstoeval.where(answer_id: answer.id).find_by_evaluation_id(evaluation2.id) || Anstoeval.new(answer_id: answer.id, evaluation_id: evaluation2.id, count: 1)
+            anstoeval2.save
+            p1=1.0*anstoeval1.count/(anstoeval1.count + anstoeval2.count)
+            @bayes[gift] = @bayes[gift]*p1/(@bayes[gift]*p1+(1-@bayes[gift])*(1-p1))
+          end
+        end
+        
+        #期待値の配列作成
+        
+        @giftExp     = Hash.new(1.0/2.0)
+        
+        #期待値と分散値の配列に値を代入
+        
+        @gifts.each do |gift|
+            # gift毎に[giftオブジェクト,期待値(Bayes更新あり)]のhash作成
+            @giftExp[gift] = 1-2*@bayes[gift]
+        end
+        
+        @expTop3=Array.new(3,Gift.find_by_id(1))
+        for i in 0..(@expTop3.length-1) do
+            @expTop3[i]=@giftExp.sort_by{|key, value| -value}[i][0]
+        end
+        
+        message=@expTop3[0].name+"\n"+@expTop3[1].name+"\n"+@expTop3[2].name
+        
         Talk.destroy_all(:user => from_mid)
       end
     end
