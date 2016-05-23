e2769e6f5782b07424ca7cca329e9d3e88bda117
Ryoto Arai <arai.ryoto@gmail.com>
2016-05-23 16:13:29 +0000

    linedelete

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160523145451) do
+ActiveRecord::Schema.define(version: 20160523154637) do
 
   create_table "anstoevals", force: :cascade do |t|
     t.integer  "answer_id"
@@ -72,6 +72,13 @@ ActiveRecord::Schema.define(version: 20160523145451) do
 
   create_table "questions", force: :cascade do |t|
     t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "talks", force: :cascade do |t|
+    t.string   "user"
+    t.string   "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
