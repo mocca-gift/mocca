3777571c0d55ab771de4acccb3489805c77a3e09
Ryoto Arai <arai.ryoto@gmail.com>
2016-05-24 18:41:11 +0000

    image

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -107,7 +107,7 @@ class WebhookController < ApplicationController
         end
         
         message=@expTop3[0].name+"\n"+@expTop3[0].url+"\n\n"+@expTop3[1].name+"\n"+@expTop3[1].url+"\n\n"+@expTop3[2].name+"\n"+@expTop3[2].url
-        res = client.sendImage([from_mid], @expTop3[0].url, "/gifts/"+@expTop3[0].id+"/img" )
+        res = client.sendImage([from_mid], @expTop3[0].url, "/gifts/"+@expTop3[0].id.to_s+"/img" )
         Talk.destroy_all(:user => from_mid)
       end
     end
