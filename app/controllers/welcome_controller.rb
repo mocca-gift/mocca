429c6bad5d2e229af58dd7fb71f90ec31423cbf9
Ryoto Arai <arai.ryoto@gmail.com>
2016-05-25 12:24:43 +0000

    Revert "should delete"
    This reverts commit 23d773494c36c99afb28949a0999da8c14e04f31.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,13 @@
 class WelcomeController < ApplicationController
+    before_action do
+        #端末によってViewファイルを振り分ける
+        case request.user_agent
+          when 'tablet'
+            request.variant = :tablet
+          when 'mobile'
+            request.variant = :mobile
+        end
+    end
     
     def index
     end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  
-  config.assets.precompile += %w(application_smart_phone.css application_smart_phone.js)
 end
