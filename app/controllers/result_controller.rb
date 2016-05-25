0debaf546d963b4c1d81ac5e1bda4d6e734c319c
Ryoto Arai <arai.ryoto@gmail.com>
2016-05-25 17:24:52 +0000

    applying to mobile

--- app/controllers/result_controller.rb
+++ app/controllers/result_controller.rb
@@ -6,7 +6,7 @@
 class ResultController < ApplicationController
     before_action do
         #端末によってViewファイルを振り分ける
-        if request.user_agent.include?("Mobile") then
+        if request.user_agent.include?("Mobile") || request.user_agent.include?("iPhone") then
             request.variant = :mobile
         else
         end
