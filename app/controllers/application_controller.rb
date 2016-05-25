d63868760a97755673f48741f7b2a270d8a82073
Ryoto Arai <arai.ryoto@gmail.com>
2016-05-25 12:29:44 +0000

    Revert "mobilestart"
    This reverts commit 097d0b77bda32ca15d8f5f3713ea51f63039bfc5.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  include Jpmobile::ViewSelector
-  
+  def hello
+    render text:"Hello World!!"
+  end
   
 end
