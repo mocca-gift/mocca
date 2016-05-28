3f24b737e28489ae7dfb6547a4b31589b5fcb0d6
Ryoto Arai <arai.ryoto@gmail.com>
2016-05-28 13:30:18 +0000

    Revert "fff"
    This reverts commit 092989bdf8be2fe80c9448af53220c898d1f1d8c.

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -1,6 +1,5 @@
 class GiftsController < ApplicationController
   before_action :set_gift, only: [:show, :edit, :update, :destroy, :img]
-  before_action :restrict_remote_ip
 
   # GET /gifts
   # GET /gifts.json
@@ -94,13 +93,6 @@ class GiftsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def gift_params
       params.require(:gift).permit(:name,:url)
-    end
-    
-    PERMIT_ADDRESSES = ['127.0.0.1', '::1', '000.000.00.00', '119.104.104.23','103.54.211.10'].freeze
-
-    def restrict_remote_ip
-    unless PERMIT_ADDRESSES.include?(request.remote_ip)
-      render text: 'Service Unavailable', status: 503
     end
 end
 
