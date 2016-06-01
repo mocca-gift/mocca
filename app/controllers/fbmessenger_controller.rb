7dda38eb90e51749a5cbe179ae2a8a978536d3db
Ryoto Arai <arai.ryoto@gmail.com>
2016-06-01 14:29:11 +0000

    nothing

--- app/controllers/fbmessenger_controller.rb
+++ app/controllers/fbmessenger_controller.rb
@@ -10,23 +10,23 @@ class FbmessengerController < ApplicationController
     
           #ユーザーの発言
     
-          sender = message["sender"]["id"]
-          text = message["message"]["text"]
+          # sender = message["sender"]["id"]
+          # text = message["message"]["text"]
     
-          endpoint_uri = "https://graph.facebook.com/v2.6/me/messages?access_token=" + token
-          request_content = {recipient: {id: sender},
-                             message: {text: text}
-                            }
+          # endpoint_uri = "https://graph.facebook.com/v2.6/me/messages?access_token=" + token
+          # request_content = {recipient: {id: sender},
+          #                   message: {text: text}
+          #                   }
     
-          content_json = request_content.to_json
+          # content_json = request_content.to_json
     
-          RestClient.post(endpoint_uri, content_json, {
-            'Content-Type' => 'application/json; charset=UTF-8'
-          }){ |response, request, result, &block|
-            p response
-            p request
-            p result
-          }
+          # RestClient.post(endpoint_uri, content_json, {
+          #   'Content-Type' => 'application/json; charset=UTF-8'
+          # }){ |response, request, result, &block|
+          #   p response
+          #   p request
+          #   p result
+          # }
         else
           #botの発言
         end
