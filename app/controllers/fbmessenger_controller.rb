1ffe01d586f91ba2f81f5b6041b8d7f2accf1dbe
Ryoto Arai <arai.ryoto@gmail.com>
2016-06-01 17:53:46 +0000

    nothing]

--- app/controllers/fbmessenger_controller.rb
+++ app/controllers/fbmessenger_controller.rb
@@ -11,6 +11,7 @@ class FbmessengerController < ApplicationController
         token = "EAAWFQPfGEBkBAEdKXCPOQ5zb28zMOeSKkZAvQjYnODIPOakcDpC92wlHWARJZA4COUu77bBzAhW4SW0ybdPLELIZC8d60EAwoMpv8DBQR1gaHiZA0WZCbhZBxDGXSOIwojLc51OCGHF9EUghwMHy4oZCV55nvU4iZBcxZAZAfEjr3jCCgedb4aZCoSa"
 
         message = params["entry"][0]["messaging"][0]
+        @gifts=Gift.order("created_at DESC").limit(2)
     
         if message.include?("message") then
     
@@ -27,26 +28,34 @@ class FbmessengerController < ApplicationController
               "payload": {
                 "template_type": "generic",
                 "elements": [{
-                  "title": "First card",
+                  "title": @gifts[0].name ,
                   "subtitle": "Element #1 of an hscroll",
-                  "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+                  "image_url": "https://mocca-giftfinder.herokuapp.com/gifts/"+@gifts[0].id+"/img",
                   "buttons": [{
                     "type": "web_url",
-                    "url": "https://www.messenger.com/",
-                    "title": "Web url"
+                    "url": @gifts[0].url ,
+                    "title": "こちらから！"
                   }, {
                     "type": "postback",
                     "title": "Postback",
                     "payload": "Payload for first element in a generic bubble",
                   }],
                 },{
-                  "title": "Second card",
+                  "title": @gifts[1].name,
                   "subtitle": "Element #2 of an hscroll",
-                  "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+                  "image_url": "https://mocca-giftfinder.herokuapp.com/gifts/"+@gifts[1].id+"/img",
                   "buttons": [{
+                    "type": "web_url",
+                    "url": @gifts[1].url ,
+                    "title": "こちらから！"
+                  }, {
                     "type": "postback",
-                    "title": "Postback",
-                    "payload": "Payload for second element in a generic bubble",
+                    "title": "いい！",
+                    "payload": "1",
+                  },{
+                    "type": "postback",
+                    "title": "びみょ！",
+                    "payload": "2",
                   }],
                 }]
               }
