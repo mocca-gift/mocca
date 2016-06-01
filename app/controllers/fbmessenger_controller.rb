635ae1a1b0d5fc86288f69963d45f486deeba489
Ryoto Arai <arai.ryoto@gmail.com>
2016-06-01 16:02:08 +0000

    nothing

--- app/controllers/fbmessenger_controller.rb
+++ app/controllers/fbmessenger_controller.rb
@@ -1,5 +1,5 @@
 class FbmessengerController < ApplicationController
-    protect_from_forgery except: :callback
+    protect_from_forgery with: :null_session
     
     def callback
         token = "EAAWFQPfGEBkBAFx7AxBpdVK4VViIEsDcpRV5fXRV1jmWmg5lg9Kj8rF2Le0nUnAfNnrLFbS5xDuEuZAwoI9xZCQ7CUf9HOvSFQaVAcF0gxen3OixKVSvVulUJxOrbP1VHmZCRNfk1qAAbYS2k88S08rUllOjbwDXSV1jXeEQH8ZAn26sdieP"
@@ -10,23 +10,23 @@ class FbmessengerController < ApplicationController
     
           #ユーザーの発言
     
-          # sender = message["sender"]["id"]
-          # text = message["message"]["text"]
+          @sender = message["sender"]["id"]
+          @text = message["message"]["text"]
     
-          # endpoint_uri = "https://graph.facebook.com/v2.6/me/messages?access_token=" + token
-          # request_content = {recipient: {id: sender},
-          #                   message: {text: text}
-          #                   }
+          endpoint_uri = "https://graph.facebook.com/v2.6/me/messages?access_token="+token
+          request_content = {recipient: {id: @sender},
+                            message: {text: @text}
+                            }
     
-          # content_json = request_content.to_json
+          content_json = request_content.to_json
     
-          # RestClient.post(endpoint_uri, content_json, {
-          #   'Content-Type' => 'application/json; charset=UTF-8'
-          # }){ |response, request, result, &block|
-          #   p response
-          #   p request
-          #   p result
-          # }
+          RestClient.post(endpoint_uri, content_json, {
+            'Content-Type' => 'application/json; charset=UTF-8'
+          }){ |response, request, result, &block|
+            p response
+            p request
+            p result
+          }
         else
           #botの発言
         end
