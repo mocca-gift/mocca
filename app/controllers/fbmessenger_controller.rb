a53c7dc1da8f059be0a5febd5045662c70079268
Ryoto Arai <arai.ryoto@gmail.com>
2016-06-01 17:33:36 +0000

    nothing]

--- app/controllers/fbmessenger_controller.rb
+++ app/controllers/fbmessenger_controller.rb
@@ -20,8 +20,41 @@ class FbmessengerController < ApplicationController
           @text = message["message"]["text"]
     
           endpoint_uri = "https://graph.facebook.com/v2.6/me/messages?access_token="+token
-          request_content = {recipient: {id: @sender},
-                            message: {text: @text}
+          
+          @messageData = {
+            "attachment": {
+              "type": "template",
+              "payload": {
+                "template_type": "generic",
+                "elements": [{
+                  "title": "First card",
+                  "subtitle": "Element #1 of an hscroll",
+                  "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+                  "buttons": [{
+                    "type": "web_url",
+                    "url": "https://www.messenger.com/",
+                    "title": "Web url"
+                  }, {
+                    "type": "postback",
+                    "title": "Postback",
+                    "payload": "Payload for first element in a generic bubble",
+                  }],
+                },{
+                  "title": "Second card",
+                  "subtitle": "Element #2 of an hscroll",
+                  "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+                  "buttons": [{
+                    "type": "postback",
+                    "title": "Postback",
+                    "payload": "Payload for second element in a generic bubble",
+                  }],
+                }]
+              }
+            }
+          }
+        
+        request_content = {recipient: {id: @sender},
+                            message: @messageData
                             }
     
           content_json = request_content.to_json
