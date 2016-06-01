d8798633f9c1d03370321b6c4772e8ea9b0a95c4
Ryoto Arai <arai.ryoto@gmail.com>
2016-06-01 11:32:02 +0000

    fboumu

--- app/controllers/fbmessenger_controller.rb
+++ app/controllers/fbmessenger_controller.rb
@@ -1,9 +1,32 @@
 class FbmessengerController < ApplicationController
     def callback
-        if params["hub.verify_token"] == "EAAWFQPfGEBkBAE71fCNh9vLkhPQF3pvDVsEgC0ak92iyYH9E9Vnin2UyEK8D3s5GqoZAFvkrZAe8pkg82SViLFB9J9ckqQBnGciInHXhqJcwfs0jLA3AF5ZAPRR7Jo9XNNKzKJcawXBlz8r9Bt2ZC8sZA6G0kLx9B1dDeNTS93wzf3LwUmFka"
-           render json: params["hub.challenge"]
+        token = "EAAWFQPfGEBkBAFx7AxBpdVK4VViIEsDcpRV5fXRV1jmWmg5lg9Kj8rF2Le0nUnAfNnrLFbS5xDuEuZAwoI9xZCQ7CUf9HOvSFQaVAcF0gxen3OixKVSvVulUJxOrbP1VHmZCRNfk1qAAbYS2k88S08rUllOjbwDXSV1jXeEQH8ZAn26sdieP"
+
+        message = params["entry"][0]["messaging"][0]    
+    
+        if message.include?("message")
+    
+          #ユーザーの発言
+    
+          sender = message["sender"]["id"]
+          text = message["message"]["text"]
+    
+          endpoint_uri = "https://graph.facebook.com/v2.6/me/messages?access_token=" + token
+          request_content = {recipient: {id:sender},
+                             message: {text: text}
+                            }
+    
+          content_json = request_content.to_json
+    
+          RestClient.post(endpoint_uri, content_json, {
+            'Content-Type' => 'application/json; charset=UTF-8'
+          }){ |response, request, result, &block|
+            p response
+            p request
+            p result
+          }
         else
-           render json: "Error, wrong validation token"
+          #botの発言
         end
     end
 
