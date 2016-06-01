0ac8ee23c06bd6b5f25872ab07f08056ed4aa523
Ryoto Arai <arai.ryoto@gmail.com>
2016-06-01 17:58:26 +0000

    nothing]

--- app/controllers/fbmessenger_controller.rb
+++ app/controllers/fbmessenger_controller.rb
@@ -77,7 +77,22 @@ class FbmessengerController < ApplicationController
           #   p result
           # }
         else
-          #botの発言
+          if message.include?("payback") then
+            @sender = message["sender"]["id"]
+            @text = message["payback"]["payload"]
+            endpoint_uri = "https://graph.facebook.com/v2.6/me/messages?access_token="+token
+            request_content = {recipient: {id: @sender},
+                            message: @text
+                            }
+    
+          content_json = request_content.to_json
+    
+          RestClient.post(endpoint_uri, content_json, {
+            'Content-Type' => 'application/json; charset=UTF-8'
+          })
+            
+          else
+          end
         end
         
         render :nothing => true, status: :ok
